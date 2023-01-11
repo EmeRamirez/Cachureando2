d54584f503cf652e12ad810ea9107738490ef089
Andrea Jota <andreacjimenez@gmail.com>
2023-01-10 21:23:32 -0300

    avance sin responsividad

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-
 var fecha = document.getElementById('fecha');
-setInterval(function() {
+setInterval(function () {
     var data = new Date();
-    fecha.innerHTML = data.getDate()+ "/" + "/" +data.getMonth()+1 + "/"+data.getFullYear()+"/ hora: "+data.toLocaleTimeString();
+    fecha.innerHTML = data.getDate()+ "/" +data.getMonth()+1 + "/"+data.getFullYear()+"/ hora: "+data.toLocaleTimeString();
 },1000);
