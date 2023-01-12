f14672f803b7489df064f7a0e28e932ee2ae638c
Andrea Jota <andreacjimenez@gmail.com>
2023-01-12 15:16:15 -0300

    separación de hojas de estilo y trabajo inicial de responsividad

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var fecha = document.getElementById('fecha');
 setInterval(function () {
     var data = new Date();
-    fecha.innerHTML = data.getDate()+ "/" +data.getMonth()+1 + "/"+data.getFullYear()+"/ hora: "+data.toLocaleTimeString();
+    fecha.innerHTML = data.toLocaleTimeString();
 },1000);
+
